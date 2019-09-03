40bb0149dc84a28b08c19a8f268c504612a1928e
Eric Newport <kethinov@gmail.com>
2019-09-03 00:01:11 -0400

    bulk dep update and perf refactor

--- cssIsDisabled.js
+++ cssIsDisabled.js
@@ -2,15 +2,16 @@ module.exports = function () {
   var cssdisabled = false // must be proven otherwise
   var testel = document.createElement('div')
   var currstyle
+  var body = document.body
   testel.style.position = 'absolute'
-  document.getElementsByTagName('body')[0].appendChild(testel)
+  body.appendChild(testel)
   if (testel.currentStyle) {
-    currstyle = testel.currentStyle['position']
+    currstyle = testel.currentStyle.position
   } else if (window.getComputedStyle) {
     currstyle = document.defaultView.getComputedStyle(testel, null).getPropertyValue('position')
   }
   cssdisabled = currstyle === 'static'
-  document.getElementsByTagName('body')[0].removeChild(testel)
+  body.removeChild(testel)
   if (cssdisabled) {
     return true
   }
